3b9c17a98b8bd72e8546e8e1bbee1da7c05980a9
h <h@>
2018-12-10 18:43:58 +0800

    Implement /health /thumb /meta

--- server/server.go
+++ server/server.go
@@ -5,21 +5,30 @@
 package server
 
 import (
+	"bytes"
 	"context"
+	//"fmt"
 
 	"../video"
+	"github.com/golang/protobuf/ptypes/empty"
 )
 
-func (s *vdoThuServiceServer) Health(context.Context, *video.HealthRequest) (*video.HealthResponse, error) {
-	return &video.HealthResponse{}, nil
+func (s *vdoThuServiceServer) Health(context.Context, *empty.Empty) (*video.HealthResponse, error) {
+	return &video.HealthResponse{Health: []byte("ok")}, nil
 }
 
-func (s *vdoThuServiceServer) Thumb(context.Context, *video.ThumbRequest) (*video.ThumbResponse, error) {
-	return &video.ThumbResponse{}, nil
+func (s *vdoThuServiceServer) Thumb(c context.Context, r *video.ThumbRequest) (*video.ThumbResponse, error) {
+	//fmt.Printf("%v\n", r)
+	buf := &bytes.Buffer{}
+	video.Thumb(buf, r)
+	return &video.ThumbResponse{Thumb: buf.Bytes()}, nil
 }
 
-func (s *vdoThuServiceServer) Meta(context.Context, *video.MetaRequest) (*video.MetaResponse, error) {
-	return &video.MetaResponse{}, nil
+func (s *vdoThuServiceServer) Meta(c context.Context, r *video.MetaRequest) (*video.MetaResponse, error) {
+	//fmt.Printf("%v\n", r)
+	buf := &bytes.Buffer{}
+	video.Meta(buf, r)
+	return &video.MetaResponse{Meta: buf.Bytes()}, nil
 }
 
 func newVdoThuServiceServer() video.VdoThuServiceServer {
